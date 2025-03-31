8f14da5d38a7ab1d745bff2ab207d1997e5c0ce7
Mikee Ojastro <mikeeojastro@gmail.com>
2025-03-31 20:39:27 +0800

    March 31, 2025

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Navigation from './components/Navigation/Navigation';
+import Products from './components/Products/Products';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      <Products />
+      <Footer />
     </div>
   );
 }
